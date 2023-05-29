2b05195676db7edb6484e69069700329379a9f77
henrypheonix2 <henryogazi@yahoo.com>
2023-05-29 04:42:07 -0700

    commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _puts - prints a string
-* @str: the string
-*
-* Return: the length of the string
-*/
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
 void _puts(char *str)
 {
 int i = 0;
